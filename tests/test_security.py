57ce40b74dc101cfe94d5870a7680ff484139aa4
OleksandrStatsenko <alexstats200486@gmail.com>
2026-05-13 14:51:42 +0300

    Update test_security.py

--- tests/test_security.py
+++ tests/test_security.py
@@ -2,6 +2,7 @@ from app import app
 
 client = app.test_client()
 
+# SQL Injection
 def test_sql_injection():
     response = client.post('/login', data={
         'username': "admin' --",
@@ -10,16 +11,19 @@ def test_sql_injection():
 
     assert 'Вітаємо!' not in response.get_data(as_text=True)
 
+# XSS
 def test_xss():
     response = client.get('/profile?name=<script>alert(1)</script>')
 
     assert '<script>' not in response.get_data(as_text=True)
 
+# Path Traversal
 def test_path_traversal():
     response = client.get('/file?name=../../etc/passwd')
 
     assert 'root:' not in response.get_data(as_text=True)
 
+# Command Injection
 def test_command_injection():
     response = client.get('/run?cmd=ls;cat /etc/passwd')
 
